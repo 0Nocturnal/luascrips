0b996789118cf55abb4836c1942a5f210afee89d
nocturnal <52535450+0Nocturnal@users.noreply.github.com>
2024-01-28 20:42:37 -0300

    Update Simple Woodcutting.lua

--- Simple Woodcutting/Simple Woodcutting.lua
+++ Simple Woodcutting/Simple Woodcutting.lua
@@ -292,7 +292,7 @@ local function dropInventory()
     for _, item in ipairs(API.ReadInvArrays33()) do
         for _, v in pairs({13435, 335, 331}) do
             if (item.itemid1 == v) then
-                API.DoAction_Inventory1(item.itemid1, 0, 8, API.OFF_ACT_GeneralInterface_route)
+                API.DoAction_Interface(0x24,0x14f,8,1473,5,item.index, API.OFF_ACT_GeneralInterface_route2)
                 API.RandomSleep2(200, 100, 200)
             end
         end
